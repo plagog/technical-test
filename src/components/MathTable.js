e5bbb6ac1f535a28575867cac44b2ad7e2fb5943
Panos Lagogiannis <panoslazpz@gmail.com>
2022-04-14 02:33:00 -0400

    expansion panel

--- src/components/MathTable.js
+++ src/components/MathTable.js
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { wordToNumber } from "../utils/WordToNumberConverter";
 import { DataGrid } from "@mui/x-data-grid";
 import { CircularProgress } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const API1 = "https://100insure.com/mi/api1.php";
 const API2 = "https://100insure.com/mi/api2.php";
@@ -22,28 +27,28 @@ const MathTable = () => {
   const [loading1, setLoading1] = useState(true);
   const [loading2, setLoading2] = useState(true);
 
-  useEffect(() => {
+  async function fetchNumbers() {
+    setNumbers([]);
+    setWords([]);
+    setResults([]);
     setLoading(true);
+    setLoading1(true);
+    const DATA = await fetch(API1, { method: "GET" });
+    const body = await DATA.json();
+    let tempArray = [];
+    let tempArray1 = [];
+    Object.values(body).forEach((value) => {
+      tempArray.push(wordToNumber(value));
+      tempArray1.push(value);
+    });
+    setWords(tempArray1);
+    setNumbers(tempArray);
 
+    setLoading1(false);
+  }
+
+  useEffect(() => {
     fetchNumbers();
-
-    async function fetchNumbers() {
-      setNumbers([]);
-      setWords([]);
-      setLoading1(true);
-      const DATA = await fetch(API1, { method: "GET" });
-      const body = await DATA.json();
-      let tempArray = [];
-      let tempArray1 = [];
-      Object.values(body).forEach((value) => {
-        tempArray.push(wordToNumber(value));
-        tempArray1.push(value);
-      });
-      setWords(tempArray1);
-      setNumbers(tempArray);
-
-      setLoading1(false);
-    }
   }, []);
 
   useEffect(() => {
@@ -81,71 +86,123 @@ const MathTable = () => {
     }
   }, [numbers, operators]);
 
+  const reset = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setNumbers([]);
+    setWords([]);
+    setResults([]);
+    fetchNumbers();
+  };
+
   return (
     <>
       <div className="console">
-        <h2>
-          Mock Console{" "}
-          <span style={{ fontSize: 12 }}>
-            Press F5 for a new set of numbers
-          </span>
-        </h2>
-
-        <ul>
-          <li>
-            Fetching and converting word numbers:
-            {loading1 && (
-              <CircularProgress
-                size={12}
-                sx={{
-                  color: "black",
-
-                  marginTop: "-12px",
-                  marginLeft: "5px",
-                }}
-              />
-            )}
-            <ul>
-              {words[0] && (
-                <li>
-                  {words[0]} -> {numbers[0]}
-                </li>
-              )}
-              {words[1] && (
-                <li>
-                  {words[1]} -> {numbers[1]}
-                </li>
-              )}
-            </ul>
-          </li>
-
-          {!loading1 && (
-            <li>
-              Fetching result for each operation
-              {loading2 && (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <span
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                alignItems: "center",
+              }}
+            >
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <h2>Mock Console</h2>
+                <span
+                  style={{
+                    fontSize: 12,
+                    fontWeight: "bold",
+                    marginLeft: "10px",
+                  }}
+                >
+                  Press F5 for a new set of numbers
+                </span>
+              </span>
+              {loading ? (
                 <CircularProgress
-                  size={12}
+                  size={24}
                   sx={{
                     color: "black",
 
-                    marginTop: "-12px",
-                    marginLeft: "5px",
+                    marginTop: "5px",
+                    marginRight: "15px",
                   }}
                 />
+              ) : (
+                <p
+                  onClick={(event) => {
+                    reset(event);
+                  }}
+                >
+                  Restart
+                </p>
               )}
-              <ul>
-                {results.map((el, i) => {
-                  return (
-                    <li key={i}>
-                      {el?.operation} -> {el?.result}
+            </span>
+          </AccordionSummary>
+          <AccordionDetails>
+            <ul>
+              <li>
+                Fetching and converting word numbers:
+                {loading1 && (
+                  <CircularProgress
+                    size={12}
+                    sx={{
+                      color: "black",
+
+                      marginTop: "-12px",
+                      marginLeft: "5px",
+                    }}
+                  />
+                )}
+                <ul>
+                  {words[0] && (
+                    <li>
+                      {words[0]} -> {numbers[0]}
                     </li>
-                  );
-                })}
-              </ul>
-            </li>
-          )}
-          {!loading1 && !loading2 && <li>{!loading && "Done"}</li>}
-        </ul>
+                  )}
+                  {words[1] && (
+                    <li>
+                      {words[1]} -> {numbers[1]}
+                    </li>
+                  )}
+                </ul>
+              </li>
+
+              {!loading1 && (
+                <li>
+                  Fetching result for each operation
+                  {loading2 && (
+                    <CircularProgress
+                      size={12}
+                      sx={{
+                        color: "black",
+
+                        marginTop: "-12px",
+                        marginLeft: "5px",
+                      }}
+                    />
+                  )}
+                  <ul>
+                    {results.map((el, i) => {
+                      return (
+                        <li key={i}>
+                          {el?.operation} -> {el?.result}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              )}
+              {!loading1 && !loading2 && <li>{!loading && "Done"}</li>}
+            </ul>
+          </AccordionDetails>
+        </Accordion>
       </div>
       <div className="main">
         <DataGrid
